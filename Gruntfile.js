240755e9cb3956625037b29e98fd633cbc7ba049
jakerella <jordan@jordankasper.com>
2014-05-12 22:10:27 -0500

    Added uglification solution

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,7 @@ module.exports = function( grunt ) {
     grunt.loadNpmTasks( "grunt-contrib-copy" );
     grunt.loadNpmTasks( "grunt-contrib-clean" );
     grunt.loadNpmTasks( "grunt-contrib-jshint" );
+    grunt.loadNpmTasks( "grunt-contrib-uglify" );
 
     grunt.initConfig({
         copy: {
@@ -26,15 +27,31 @@ module.exports = function( grunt ) {
 
         jshint: {
             options: {
-                jshintrc: '.jshintrc',
-                jshintignore: '.jshintignore'
+                jshintrc: ".jshintrc",
+                jshintignore: ".jshintignore"
             },
             files: {
                 src: [ "src/js/**/*.js" ]
             }
         },
+
+        uglify: {
+            app: {
+                options: {
+                    sourceMap: true,
+                    sourceMapName: "dist/js/sourcemap.map"
+                },
+                files: {
+                    "dist/js/beers.min.js": [
+                        "dist/js/app.js",
+                        "dist/js/models/*.js",
+                        "dist/js/viewModels/*.js"
+                    ]
+                }
+            }
+        }
     });
 
-    grunt.registerTask( "default", [ "clean", "jshint", "copy" ] );
+    grunt.registerTask( "default", [ "clean", "jshint", "copy", "uglify" ] );
 
 };
