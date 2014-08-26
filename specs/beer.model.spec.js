b18c37aeb97834095e68bef10cd7ce561b05888f
Jordan Kasper <jordan@jordankasper.com>
2014-08-26 14:38:24 -0400

    Chnage global to window

--- specs/beer.model.spec.js
+++ specs/beer.model.spec.js
@@ -1,9 +1,9 @@
 QUnit.test("Confirm model exists", function(assert) {
-    assert.ok(global.beerApp.Beer, "The Beer model exists on the beerApp object");
+    assert.ok(window.beerApp.Beer, "The Beer model exists on the beerApp object");
 });
 
 QUnit.test("Empty Beer object gets defaults", function(assert) {
-    var b = new global.beerApp.Beer();
+    var b = new window.beerApp.Beer();
     assert.ok(b, "The Beer object was created");
     assert.equal(b.name, "Beer", "The default name was used");
 });
